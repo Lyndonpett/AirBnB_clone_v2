217342e09c81d6429759782edcbdb96d0f73dd41
lyndonpett <lyndonpett98@gmail.com>
2021-12-13 11:06:33 -0600

    Adding task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,7 +3,9 @@
 
 
 from flask import Flask, render_template
-from models import storage, State, Amenity
+from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 
 app = Flask(__name__)
