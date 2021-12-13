df544b7e0168eaf98fa3a037ae605a4410ce593e
lyndonpett <lyndonpett98@gmail.com>
2021-12-13 08:50:55 -0600

    Test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,41 +1,56 @@
 #!/usr/bin/python3
-'''Starts a Flask Web app'''
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello():
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def display():
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def cText(text):
-    return 'C {}'.format(text.replace('_', ' '))
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythonText(text='is cool'):
-    return 'Python {}'.format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def numInt(n):
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def numTemplate(n):
-    return render_template('5-number.html', n=n)
-
+"""[This Starts the Flask Application]
+"""
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    from flask import Flask
+    from flask import render_template
+
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+
+    @app.route('/')
+    def hello_HBNB():
+        """[This stes the route for hello_hbnb]
+        """
+        return "Hello HBNB!"
+
+    @app.route('/hbnb')
+    def HBNB():
+        """[This route displays HBHB]
+        """
+        return "HBNB"
+
+    @app.route('/c/<text>')
+    def print_C(text):
+        """[/c/<text> Displays C followed by the value of the text]
+        """
+        new_str = text.replace('_', ' ')
+        return "C {}".format(new_str)
+
+    @app.route('/python/', defaults={'text': 'is cool'})
+    @app.route('/python/<text>')
+    def print_python(text="is cool"):
+        """[Prints python and gets rid of '_'
+            if there are any]
+        Args:
+            text (str):Defaults to "is cool".
+        """
+        new_str = text.replace('_', ' ')
+        return "Python {}".format(new_str)
+
+    @app.route('/number/<int:n>')
+    def display_n(n):
+        """[display “n is a number” only if n is an integer]
+        Args:
+            n ([int]):
+        """
+        return "{} is a number".format(n)
+
+    @app.route('/number_template/<int:n>')
+    def number_template(n):
+        """[This displays h1 tag]
+        """
+        return render_template('5-number.html', n=n)
+
+    app.run(host='0.0.0.0', port='5000')
