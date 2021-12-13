ed0c6bd7558f1f8b8c600261813a2ec62751da7e
lyndonpett <lyndonpett98@gmail.com>
2021-12-13 09:03:13 -0600

    Adding task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Flask app for states'''
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def statesList():
+    state = storage.all(State)
+    return render_template('8-states_list.html', States=state)
+
+
+@app.teardown_appcontext
+def teardown(context):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
